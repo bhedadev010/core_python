3227fede1e16b4c2287a27f4e8122a14d7d76bbf
Dev Bheda <bhedadev010@gmail.com>
2024-07-13 22:51:59 +0530

    demo

---
+++ for-loop_variations.py
@@ -0,0 +1,19 @@
+#1 way
+
+for i in range(10):
+    print(i)
+
+#2 way
+
+for i in range(2,10):
+    print(i)
+
+#3 way
+
+for i in range(3,10,2):
+    print(i)
+
+#4 way
+
+for i in range(10,0,-2):
+    print(i)
