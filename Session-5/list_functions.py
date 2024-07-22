5f462b402dbb48336849f918e1e7ea83307a1d69
Dev Bheda <bhedadev010@gmail.com>
2024-07-22 08:33:57 +0530

    20-7-24

---
+++ Session-5/list_functions.py
@@ -0,0 +1,33 @@
+l=[1,2,4,100,'tops',True,False,1] #TRUE is 1 and FALSE is 0
+m=[1,4,3,6,5,7,5]
+
+l.append(3)
+print(l) #Adds a single element at the end of the list
+
+l.pop(5)
+print(l) #Removes a single element of whos index is passed
+
+print(l.index('tops')) #Gives index value of the arguement given in brackets
+
+print(l.count(1)) #Counts the occurance of a given arguement in the list
+
+# l.clear()
+# print(l) #Delete all the elements of the list
+
+m=l.copy()  #Copies the list in the list it points to
+m.append('dev')
+print(m)
+
+l.insert(3,'wow')  #Inserts a given object to the given index of the list
+print(l)
+
+l.remove('wow') #Deletes the object which is passed from the list
+print(l)
+
+l.reverse()
+print(l)
+
+l.__delitem__(6) #Deletes the object from the index passed as arguement
+print(l)
+
+
---
+++ Session-5/list_slicing.py
@@ -0,0 +1,17 @@
+l=[1,2,3,1.1,'tops',5,6,7,51,False,20,40,30,50]
+
+print(l[13:15])
+print(l[:2])
+print(l[10::10])
+print(l[::15])
+print(l[:10:7])
+
+print(l[-15:-12])
+print(l[:-15])
+print(l[-12::9])
+print(l[::-10])
+print(l[:-10:2])
+
+
+#please understand blank positions in negative signing YOU DONT KNOW IT
+#made mistake in nprevious ones too
